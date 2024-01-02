12cd44e0afdde46b4cd191b39458083a6b33e915
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-01-02 16:37:06 -0400

    feat: Avançando no admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,8 +4,11 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class ListaFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter= ("categoria",)
+    list_editable= ("publicada",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListaFotografias)
